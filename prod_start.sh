ce5e76bcc9d70f2d22b602d96f0eb69953512b44
Andrey Smetanin <irqlevel@gmail.com>
2015-09-06 00:49:14 +0300

    tune production start script

--- prod_start.sh
+++ prod_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -xv
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-./activator clean stage
+$DIR/activator clean stage
 secret="$(openssl rand -base64 32)"
-target/universal/stage/bin/myapp -Dplay.crypto.secret=$secret -Dhttp.port=80 -Dhttp.address=0.0.0.0 -Dpidfile.path=$DIR/prod_pid
+nohup $DIR/target/universal/stage/bin/myapp -Dplay.crypto.secret=$secret -Dhttp.port=80 -Dhttp.address=0.0.0.0 -Dpidfile.path=$DIR/prod_pid > prod_out.log 2>&1 &
+
