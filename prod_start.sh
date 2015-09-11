93e7f4dd873458b0f943b57d7f0c89f1a19fc09f
Andrey Smetanin <irqlevel@gmail.com>
2015-09-11 17:10:21 +0300

    nginx integration

--- prod_start.sh
+++ prod_start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -xv
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+NGINX_DIR=/root/nginx
 $DIR/activator clean stage
 secret="$(openssl rand -base64 32)"
-nohup $DIR/target/universal/stage/bin/myapp -Dplay.crypto.secret=$secret -Dhttp.port=80 -Dhttp.address=0.0.0.0 -Dpidfile.path=$DIR/prod_pid > prod_out.log 2>&1 &
-
+nohup $DIR/target/universal/stage/bin/myapp -Dplay.crypto.secret=$secret -Dhttp.port=8080 -Dhttp.address=127.0.0.1 -Dpidfile.path=$DIR/prod_pid > prod_out.log 2>&1 &
+nohup $NGINX_DIR/sbin/nginx > nginx.log 2>&1 &
--- prod_stop.sh
+++ prod_stop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -xv
+NGINX_DIR=/root/nginx
+NGINX_PID="$(cat $NGINX_DIR/logs/nginx.pid)"
+kill $NGINX_PID
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-if [ ! -f $DIR/prod_pid ]; then
-	echo "no pid exists"
-	exit
-fi
 PID="$(cat $DIR/prod_pid)"
 kill $PID
